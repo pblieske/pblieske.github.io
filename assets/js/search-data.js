5b973bfe101ecdf7dccc869fa937f49395976527
pblieske <159456965+pblieske@users.noreply.github.com>
2025-12-25 17:01:26 +0000

    Deploying to gh-pages from @ pblieske/pblieske.github.io@7000302160a34de8e1133c343efcf05683a22a2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,8 +42,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-deployed-my-new-webpage",
+          title: 'Deployed my new webpage!',
           description: "",
           section: "News",},{id: "projects-upcoming",
           title: 'Upcoming',
