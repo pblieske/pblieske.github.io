b747e80c3577e122c4f4470509e24a35e5962937
pblieske <159456965+pblieske@users.noreply.github.com>
2026-07-05 14:11:55 +0000

    Deploying to gh-pages from @ pblieske/pblieske.github.io@37102a8d11aaf0b38b856847c813db095b2d6945 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
